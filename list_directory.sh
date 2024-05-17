e105be74c936130f18052e0af40a92107661d1a3
Zeyad Mohamed Ali <103035624+ZeyadMoAli@users.noreply.github.com>
2024-05-17 17:48:23 +0300

    Update list_directory.sh

--- list_directory.sh
+++ list_directory.sh
@@ -1,3 +1,13 @@
-#!/bin/bash
-echo off
-
+pipeline {
+    agent any
+    stages {
+        stage('List Directory') {
+            steps {
+                script {
+                    def output = sh(script: 'ls', returnStdout: true).trim()
+                    echo output
+                }
+            }
+        }
+    }
+}
